90e7d6886ee2062910cd09907f83263c90ec87fe
Solmorn <matwey.solomatin2007@gmail.com>
2025-09-05 22:21:22 +0300

    fixing branch

--- main.cpp
+++ main.cpp
@@ -9,22 +9,28 @@
 
 
 int main() {
-    char str1[] = "aaadedcba";
-    char str2[30] = "aa";
+    char str1[] = "2345";
+    char str2[] = "aaaaaaaaaa";
+    size_t size = 50;
+    size_t* ptr = &size;
+
+    my_getline(&str2), ptr, stdin);
+
     //strncpy(str2, str1, 4);
 
     printf("%s\n", str1);
     printf("%s\n", str2);
-    printf("%d\n", my_strspn(str1, str2));
+    printf("%d\n", my_atoi(str1));
     //printf("%c\n", *strrchr(str2, 'a'));
 }
 
 
 void my_strcpy(char* dest, const char* str) {
     assert(dest != nullptr);
-    assert(str != nullptr);
+    assert(str  != nullptr);
 
     size_t i = 0;
+
     while (str[i] != '\0') {
         dest[i] = str[i];
         i++;
@@ -33,19 +39,19 @@ void my_strcpy(char* dest, const char* str) {
 
 void my_strncpy(char* dest, const char* str, size_t n) {
     assert(dest != nullptr);
-    assert(str != nullptr);
+    assert(str  != nullptr);
 
     size_t ready = 0;
 
-    for (size_t i = 0; i < n && str[i] != '\0'; i++) {
-        dest[i] = str[i];
+    while (ready < n && str[ready] != '\0') {
+        dest[ready] = str[ready];
         ready++;
     }
 
     if (ready < n) {
 
-        for (size_t i = ready; i < n; i++) {
-            dest[i] = '\0';
+        while (ready < n) {
+            dest[ready] = '\0';
             ready++;
         }
     }
@@ -53,7 +59,7 @@ void my_strncpy(char* dest, const char* str, size_t n) {
 
 char* my_strcat(char* dest, const char* str) {
     assert(dest != nullptr);
-    assert(str != nullptr);
+    assert(str  != nullptr);
 
     size_t pointer = 0;
 
@@ -68,7 +74,7 @@ char* my_strcat(char* dest, const char* str) {
 
 char* my_strncat(char* dest, const char* str, size_t n) {
     assert(dest != nullptr);
-    assert(str != nullptr);
+    assert(str  != nullptr);
 
     size_t pointer = 0;
 
@@ -86,12 +92,14 @@ int my_strcmp(const char* str1, const char* str2) {
     assert(str2 != nullptr);
 
     for (size_t i = 0; str1[i] != '\0' || str2[i] != '\0'; i++) {
+        char c1 = str1[i];
+        char c2 = str2[i];
 
-        if (str1[i] < str2[i]) {
+        if (c1 < c2) {
             return -1;
         }
 
-        if (str1[i] > str2[i]) {
+        if (c1 > c2) {
             return 1;
         }
     }
@@ -103,13 +111,15 @@ int my_strncmp(const char* str1, const char* str2, size_t n) {
     assert(str1 != nullptr);
     assert(str2 != nullptr);
 
-    for (size_t i = 0; i < n && (str1[i] != '\0' || str2[i] != '\0'); i++) {
+    for (size_t i = 0; i < n && (str1[i] != '\0' || str2[i] != '\0'); i++) {//
+        char c1 = str1[i];
+        char c2 = str2[i];
 
-        if (str1[i] < str2[i]) {
+        if (c1 < c2) {
             return -1;
         }
 
-        if (str1[i] > str2[i]) {
+        if (c1 > c2) {
             return 1;
         }
     }
@@ -117,20 +127,20 @@ int my_strncmp(const char* str1, const char* str2, size_t n) {
     return 0;
 }
 
-char* my_strchr(char* str, int c) { //костыль
+char* my_strchr(const char* str, int c) {
     assert(str != nullptr);
 
     for (size_t i = 0; str[i] != '\0'; i++) {
 
         if (str[i] == c) {
-            return &str[i];
+            return (char*) str + i;
         }
     }
 
     return nullptr;
 }
 
-char* my_strrchr(char* str, int c) { //костыль
+char* my_strrchr(const char* str, int c) {
     assert(str != nullptr);
 
     char* res = nullptr;
@@ -138,27 +148,32 @@ char* my_strrchr(char* str, int c) { //костыль
     for (size_t i = 0; str[i] != '\0'; i++) {
 
         if (str[i] == c) {
-            res = &str[i];
+            res = (char*) str + i;
         }
     }
 
-    return res;
+    return (char*) res;
 }
 
 size_t my_strspn(const char* str, const char* allowed) {
-    assert(str != nullptr);
+    assert(str     != nullptr);
     assert(allowed != nullptr);
 
     size_t accepted_len = 0;
 
+
     while (str[accepted_len] != '\0') {
+        bool is_valid = 0;
 
         for (size_t j = 0; allowed[j] != '\0'; j++) {
 
-            if (str[accepted_len] != allowed[j]) {
-                return accepted_len;
+            if (str[accepted_len] == allowed[j]) {
+                is_valid = 1;
             }
         }
+
+        if (!is_valid) return accepted_len;
+
         accepted_len++;
     }
 
@@ -166,14 +181,14 @@ size_t my_strspn(const char* str, const char* allowed) {
 }
 
 size_t my_strcspn(const char* str, const char* not_allowed) {
-    assert(str != nullptr);
+    assert(str         != nullptr);
     assert(not_allowed != nullptr);
 
     size_t accepted_len = 0;
 
     while (str[accepted_len] != '\0') {
 
-        for (size_t j = 0; allowed[j] != '\0'; j++) {
+        for (size_t j = 0; not_allowed[j] != '\0'; j++) {
 
             if (str[accepted_len] == not_allowed[j]) {
                 return accepted_len;
@@ -185,11 +200,14 @@ size_t my_strcspn(const char* str, const char* not_allowed) {
     return accepted_len;
 }
 
-char* my_fgets(char *s, int n, FILE *iop) {
-    int c = 0;
-    char *cs = s;
+char* my_fgets(char* str, int n, FILE* file_adr) {
+    assert(str      != nullptr);
+    assert(file_adr != nullptr);
 
-    while (--n > 0 && (c = fgetc(iop)) != EOF) {
+    int   c  = 0;
+    char *cs = str;
+
+    while (--n > 0 && (c = fgetc(file_adr)) != EOF) {
 
         if ((*cs++ = c) == '\n' ) {
             break;
@@ -197,28 +215,105 @@ char* my_fgets(char *s, int n, FILE *iop) {
     }
     *cs = '\0';
 
-    return (c == EOF && cs == s) ? NULL : s;
+    return (c == EOF && cs == str) ? NULL : str;
 }
 
-int my_puts(char *s) {
-    int c = 0;
+int my_puts(char* str) {
+    assert(str != nullptr);
 
-    while (c = *s++) {
-        fputc(c, stdin);
+    for (size_t i = 0; str[i] != '\0'; i++) {
+        fputc(str[i], stdin);
     }
+
     fputc('\n', stdin);
 
     return 0;
 }
 
-char *my_strdup(char *s) {
-    char *pointer = nullptr;
+char* my_strdup(char* s) {
+    assert(s != nullptr);
+    char* pointer = nullptr;
 
-    pointer = (char *) malloc(strlen(s)+1); //поставить каллок?
+    pointer = (char*) calloc(strlen(s)+1, sizeof(s));
     if (pointer != NULL) {
-        strcpy(pointer, s);
+        my_strcpy(pointer, s);
     }
 
     return pointer;
 }
+
+int my_atoi(const char* str) {
+    assert(str != nullptr);
+
+    bool is_negative = 0;
+    int res = 0;
+    const char* allowed = " -0123456789";
+
+    if (strlen(str) != my_strspn(str, allowed)) {
+        return 0;
+    }
+
+    while (*str == ' ') {
+        str++;
+    }
+
+    if (*str == '-') {
+        is_negative = 1;
+        str++;
+    }
+
+    while (*str != '\0') {
+        char el = *str;
+
+        if ((el >= '0') && (el <= '9')) {
+            res = res * 10 + el - '0';
+        } else {
+            return 0;
+        }
+        str++;
+    }
+
+    return (is_negative) ? -res : res;
+}
+
+ssize_t my_getline(char** lineptr, size_t* n, FILE* stream) {
+    assert(lineptr != nullptr);
+    assert(stream  != nullptr);
+    assert(n       != nullptr);
+
+    char c = 0;
+    size_t filled = 0;
+
+    if (*lineptr == nullptr) {
+        *lineptr = (char*) calloc(100, 100);
+        *n = 100;
+    }
+
+    while ((c = fgetc(stream)) != '\n') {
+        printf("%c\n", c);
+
+        printf("AAA1AAAAA\n");
+
+        if (c == EOF) return -1;
+        printf("AAA2AAAAA\n");
+
+        //if (filled == *n) {
+        //    *lineptr = (char*) realloc(*lineptr, 2*filled);
+        //    *n *= 2;
+        //}
+        printf("AAAA3AAAA\n");
+
+        (*lineptr)[filled] = c;
+        filled++;
+
+        printf("AAAA3fthtttAAAA\n");
+    }
+
+    (*lineptr)[filled]   = '\n';
+    (*lineptr)[filled+1] = '\0';
+
+    return filled;
+
+}
+
 
